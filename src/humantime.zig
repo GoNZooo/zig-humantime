32f9283fb6c4b51a948a0165fcd4c4884323b531
Rickard Andersson <gonz@severnatazvezda.com>
2019-08-07 17:59:13 +0300

    switch functions to not be comptime required

--- src/humantime.zig
+++ src/humantime.zig
@@ -2,27 +2,24 @@ const std = @import("std");
 const testing = std.testing;
 const fmt = std.fmt;
 
-pub fn seconds(comptime human_string: []const u8) comptime_int {
+pub fn seconds(human_string: []const u8) u64 {
     return humanStringToInt(human_string, 1);
 }
 
-pub fn milliseconds(comptime human_string: []const u8) comptime_int {
+pub fn milliseconds(human_string: []const u8) u64 {
     return humanStringToInt(human_string, 1000);
 }
 
-fn humanStringToInt(comptime human_string: []const u8, comptime multiplier: comptime_int) u32 {
-    comptime var current_number = 0;
-    comptime var total_time = 0;
+fn humanStringToInt(human_string: []const u8, multiplier: u32) u32 {
+    var current_number: u32 = 0;
+    var total_time: u32 = 0;
 
     for (human_string) |s| {
         switch (s) {
             '0' => current_number *= 10,
             '1'...'9' => {
-                const digit = comptime fmt.charToDigit(s, 10) catch |err| {
-                    switch (err) {
-                        error.InvalidCharacter => @compileError("Bad digit in human time string: " ++ human_string),
-                    }
-                };
+                // pre-requisite already checked in the switch, fatal error if we error out on this
+                const digit = fmt.charToDigit(s, 10) catch unreachable;
                 current_number *= 10;
                 current_number += digit;
             },
@@ -46,35 +43,35 @@ fn humanStringToInt(comptime human_string: []const u8, comptime multiplier: comp
 }
 
 test "1h2m3s" {
-    const time = seconds("1h2m3s");
+    const time = comptime seconds("1h2m3s");
     testing.expectEqual(time, 3723);
 }
 
 test "2h4m6s" {
-    const time = seconds("2h4m6s");
+    const time = comptime seconds("2h4m6s");
     testing.expectEqual(time, (3600 * 2) + (4 * 60) + 6);
 }
 
 test "1h2m3s in milliseconds" {
     const format_string = "1h2m3s";
-    const time = milliseconds(format_string);
-    testing.expectEqual(time, seconds(format_string) * 1000);
+    const time = comptime milliseconds(format_string);
+    testing.expectEqual(time, comptime seconds(format_string) * 1000);
 }
 
 test "2h4m6s in milliseconds" {
     const format_string = "2h4m6s";
-    const time = milliseconds(format_string);
-    testing.expectEqual(time, seconds(format_string) * 1000);
+    const time = comptime milliseconds(format_string);
+    testing.expectEqual(time, comptime seconds(format_string) * 1000);
 }
 
 test "2h" {
-    testing.expectEqual(seconds("2h"), 7200);
+    testing.expectEqual(comptime seconds("2h"), 7200);
 }
 
 test "2m5s" {
-    testing.expectEqual(seconds("2m5s"), 125);
+    testing.expectEqual(comptime seconds("2m5s"), 125);
 }
 
 test "2h5s" {
-    testing.expectEqual(seconds("2h5s"), 7205);
+    testing.expectEqual(comptime seconds("2h5s"), 7205);
 }
